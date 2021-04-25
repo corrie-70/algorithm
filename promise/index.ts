bcf60d81db1b8d71e362e89291859f606485815d
kehuang <kehuang1013@gmail.com>
2021-04-25 21:01:07 +0800

    promise实现

--- promise/index.ts
+++ promise/index.ts
@@ -8,7 +8,7 @@ class MyPromise<T> {
     private onFulfilled: Function[] = [];
     /** 执行失败回调 */
     private onRejected: Function[] = [];
-    
+
     constructor(
         executor: (
             resolve: (value: T) => void,
@@ -41,7 +41,61 @@ class MyPromise<T> {
         }
     }
 
-    then() {}
+    private resolvePromise(promise, x, resolve, reject) {
+        if (promise === x) {
+            reject(new TypeError("Chaining cycle"));
+        }
+    }
+
+    then(onFulfilled?: Function, onRejected?: Function): MyPromise<T> {
+        const fulfilled =
+            typeof onFulfilled === "function" ? onFulfilled : (value) => value;
+        const rejected =
+            typeof onRejected === "function" ? onFulfilled : (reason) => reason;
+        const res = new MyPromise<T>((resolve, reject) => {
+            if (this.status === "fulfilled") {
+                setTimeout(() => {
+                    try {
+                        const x = fulfilled(this.value);
+                        this.resolvePromise(res, x, resolve, reject);
+                    } catch (error) {
+                        reject(error);
+                    }
+                }, 0);
+            } else if (this.status === "rejected") {
+                setTimeout(() => {
+                    try {
+                        const x = rejected(this.reason);
+                        this.resolvePromise(res, x, resolve, reject);
+                    } catch (error) {
+                        reject(error);
+                    }
+                }, 0);
+            } else {
+                this.onFulfilled.push(() => {
+                    setTimeout(() => {
+                        try {
+                            const x = fulfilled(this.value);
+                            this.resolvePromise(res, x, resolve, reject);
+                        } catch (error) {
+                            reject(error);
+                        }
+                    }, 0);
+                });
+                this.onRejected.push(() => {
+                    setTimeout(() => {
+                        try {
+                            const x = rejected(this.reason);
+                            this.resolvePromise(res, x, resolve, reject);
+                        } catch (error) {
+                            reject(error);
+                        }
+                    }, 0);
+                });
+            }
+        });
+        return res;
+    }
 }
 
 const a = new Promise((resolve) => resolve(1));
