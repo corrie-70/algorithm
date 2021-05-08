cc2afed8b84cee0b285b4c49a54eac640596bd31
kehuang <kehuang1013@gmail.com>
2021-05-08 18:30:56 +0800

    promise手写实现

--- promise/index.ts
+++ promise/index.ts
@@ -16,6 +16,7 @@ class MyPromise<T> {
         ) => void
     ) {
         this.status = "pending";
+
         try {
             executor(this.resolve, this.reject);
         } catch (e) {
@@ -23,35 +24,16 @@ class MyPromise<T> {
         }
     }
 
-    private resolve(value: T) {
-        if (this.status === "pending") {
-            this.status = "fulfilled";
-            this.value = value;
-            // 执行成功回调
-            this.onFulfilled.forEach((fn) => fn());
-        }
-    }
-
-    private reject(reason?: any) {
-        if (this.status === "pending") {
-            this.status = "rejected";
-            this.reason = reason;
-            // 执行成功回调
-            this.onRejected.forEach((fn) => fn());
-        }
-    }
-
-    private resolvePromise(promise, x, resolve, reject) {
-        if (promise === x) {
-            reject(new TypeError("Chaining cycle"));
-        }
-    }
-
     then(onFulfilled?: Function, onRejected?: Function): MyPromise<T> {
         const fulfilled =
             typeof onFulfilled === "function" ? onFulfilled : (value) => value;
         const rejected =
-            typeof onRejected === "function" ? onFulfilled : (reason) => reason;
+            typeof onRejected === "function"
+                ? onFulfilled
+                : (reason) => {
+                      throw reason;
+                  };
+
         const res = new MyPromise<T>((resolve, reject) => {
             if (this.status === "fulfilled") {
                 setTimeout(() => {
@@ -82,6 +64,7 @@ class MyPromise<T> {
                         }
                     }, 0);
                 });
+
                 this.onRejected.push(() => {
                     setTimeout(() => {
                         try {
@@ -96,6 +79,74 @@ class MyPromise<T> {
         });
         return res;
     }
+
+    private resolve = (value: T) => {
+        if (this.status === "pending") {
+            this.status = "fulfilled";
+            this.value = value;
+
+            // 执行成功回调
+            this.onFulfilled.forEach((fn) => fn());
+        }
+    };
+
+    private reject = (reason?: any) => {
+        if (this.status === "pending") {
+            this.status = "rejected";
+            this.reason = reason;
+
+            // 执行成功回调
+            this.onRejected.forEach((fn) => fn());
+        }
+    };
+
+    private resolvePromise = (promise, x, resolve, reject) => {
+        if (promise === x) {
+            reject(new TypeError("Chaining cycle"));
+        }
+
+        if (x && (typeof x === "object" || typeof x === "function")) {
+            let used = false;
+            try {
+                let thenable = x.then;
+
+                if (typeof thenable === "function") {
+                    // x为promise
+                    thenable.call(
+                        x,
+                        (y) => {
+                            if (used) return;
+
+                            used = true;
+                            this.resolvePromise(promise, y, resolve, reject);
+                        },
+                        (r) => {
+                            if (used) return;
+
+                            used = true;
+                            reject(r);
+                        }
+                    );
+                } else {
+                    resolve(x);
+                }
+            } catch (error) {
+                if (used) return;
+
+                used = true;
+                reject(error);
+            }
+        } else {
+            resolve(x);
+        }
+    };
 }
 
-const a = new Promise((resolve) => resolve(1));
+const a = new MyPromise((resolve, reject) => {
+    if (false) {
+        resolve(1);
+    }
+    reject("error");
+});
+console.log(a);
+a.then((res) => console.log("res", res));
