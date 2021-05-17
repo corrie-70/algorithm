3041115a28bcae3b6786d8f4cb95d7d515674e36
kehuang <kehuang1013@gmail.com>
2021-05-17 11:03:08 +0800

    快速排序实现

--- sort/quickSort.ts
+++ sort/quickSort.ts
@@ -6,11 +6,40 @@
  * 时间复杂度O(nlogn)，非稳定排序，原地排序
  */
 function quickSort(arr: number[]) {
-    partion(arr, 0, arr.length - 1);
+    _quickSort(arr, 0, arr.length - 1);
 }
 
-function partion(arr: number[], left: number, right: number): number {
-    let index = left;
+function _quickSort(arr: number[], startIndex: number, endIndex: number) {
+    if (endIndex <= startIndex) {
+        return [];
+    }
+
+    const pivotIndex = partion(arr, startIndex, endIndex);
+
+    _quickSort(arr, startIndex, pivotIndex - 1);
+    _quickSort(arr, pivotIndex + 1, endIndex);
+}
+
+function partion(arr: number[], startIndex: number, endIndex: number): number {
+    // return holePartion(arr, startIndex, endIndex);
+    return pointerPartion(arr, startIndex, endIndex);
+}
+
+/**
+ * 挖坑法分区定基准位置
+ * @param arr
+ * @param left
+ * @param right
+ * @returns
+ */
+function holePartion(
+    arr: number[],
+    startIndex: number,
+    endIndex: number
+): number {
+    let index = startIndex,
+        left = startIndex,
+        right = endIndex;
 
     const pivot = arr[index];
 
@@ -43,4 +72,42 @@ function partion(arr: number[], left: number, right: number): number {
     return index;
 }
 
-// console.log(partion([4, 7, 6, 5, 3, 2, 8, 1]));
+/**
+ * 指针法定分区、基准位置
+ * @param arr
+ * @param left
+ * @param right
+ */
+function pointerPartion(
+    arr: number[],
+    startIndex: number,
+    endIndex: number
+): number {
+    let left = startIndex,
+        right = endIndex;
+
+    const pivot = arr[left];
+
+    while (left !== right) {
+        while (left < right && arr[right] > pivot) {
+            right--;
+        }
+
+        while (left < right && arr[left] <= pivot) {
+            left++;
+        }
+
+        if (left < right) {
+            [arr[left], arr[right]] = [arr[right], arr[left]];
+        }
+    }
+
+    [arr[left], arr[startIndex]] = [arr[startIndex], arr[left]];
+
+    return left;
+}
+
+const test = [4, 7, 6, 5, 3, 2, 8, 1];
+quickSort(test);
+
+console.log(test);
--- sort/readme.md
+++ sort/readme.md
@@ -3,3 +3,4 @@
 ## 参考文献
 
 [必学十大经典排序算法，看这篇就够了](https://zhuanlan.zhihu.com/p/57088609)
+[漫画：什么是快速排序？](https://www.cxyxiaowu.com/5262.html)
